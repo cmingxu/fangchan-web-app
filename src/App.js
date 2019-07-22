17fd13c2f772e028f768ef648b0d6fee39408838
kevin xu <cming.xu@gmail.com>
2019-07-22 23:03:32 +0800

    text change
    Signed-off-by: kevin xu <cming.xu@gmail.com>

--- src/App.js
+++ src/App.js
@@ -44,9 +44,9 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Nav.Link href="#home">主页</Nav.Link>
+            <Nav.Link href="#link">设置</Nav.Link>
+            <NavDropdown title="更多" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -59,8 +59,8 @@ function App() {
             </NavDropdown>
           </Nav>
           <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
+            <FormControl type="text" placeholder="搜索" className="mr-sm-2" />
+            <Button variant="outline-success">搜索定位</Button>
           </Form>
         </Navbar.Collapse>
       </Navbar>
